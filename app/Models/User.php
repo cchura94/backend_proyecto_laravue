a2085529d0839d2cb447ad6f746ae44afd2524c4
cristian <cchura.cpc@gmail.com>
2024-04-05 22:46:49 -0400

    relacion N:M

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
     public function persona(){
         return $this->hasOne(Persona::class);
     }
+
+    public function roles(){
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
 }
--- docs/comandos-laravel.md
+++ docs/comandos-laravel.md
@@ -8,4 +8,7 @@ php artisan migrate:status
 php artisan migrate:rollback
 
 php artisan tinker
+------------------------------
+php artisan make:model Role -m
+php artisan make:migration create_role_user_table
 ```
